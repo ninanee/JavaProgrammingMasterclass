02ff85ad2eeb361539f7892e8027b12f992db96e
Yun Ni <71697803+ninanee@users.noreply.github.com>
2021-09-12 20:45:10 -0400

    Add files via upload

---
+++ Banking/Bank.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+
+public class Bank {
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String name) {
+        if(findBranch(name) == null){
+            this.branches.add(new Branch(name));
+            return true;
+        }
+       return false;
+    }
+
+    public boolean addCustomer(String nameOfBranch, String nameOfCustomer, double initialAmount) {
+        Branch branch = findBranch(nameOfBranch);
+        if(branch!= null) {
+            return branch.newCustomer(nameOfCustomer,initialAmount); //connect branch的method
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String nameOfBranch, String nameOfCustomer, double amount) {
+        Branch branch = findBranch(nameOfBranch);
+        if(branch!= null) {
+            return branch.addCustomerTransaction(nameOfCustomer,amount);
+        }
+        return false;
+    }
+
+    public boolean listCustomers(String nameOfBranch,boolean transaction) {
+        Branch branch = findBranch(nameOfBranch);
+        if(branch != null) {
+            System.out.println("Customer details for branch " + branch.getName());
+
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+            for(int i = 0; i < branchCustomers.size(); i++){
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i+1) +"]");
+                if(transaction) {
+                    System.out.println("Transactions");
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+                    for(int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j+1) + "] Amount " + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        }else {
+            return false;
+        }
+
+    }
+
+
+    private Branch findBranch(String branchName) {
+         for(int i = 0; i < this.branches.size(); i++) {
+             Branch checkedBranch = this.branches.get(i);
+             if(checkedBranch.getName().equals(branchName)){
+                 return checkedBranch;
+             }
+         }
+         return null;
+    }
+}
